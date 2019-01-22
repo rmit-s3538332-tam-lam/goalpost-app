081c934a07fb0c9ac22dbfb1a98413a4b331b5f6
Graphene <graphene@Graphene.local>
2019-01-22 11:02:37 +0700

    First commit

---
+++ goalpost-app/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Graphene on 22/1/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
