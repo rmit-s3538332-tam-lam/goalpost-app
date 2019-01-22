d8fb88741e1362809f46ad613ad48b00621a5674
Graphene <graphene@Graphene.local>
2019-01-22 14:50:53 +0700

    Complete table view code and enum GoalType

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
 class GoalsVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
         print("button was pressed")
     }
+    
+}
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else{
+            return UITableViewCell()
+        }
+        cell.configureCell(description: "Eat salad twice a week", type: .shortTerm, goalProgressAmount: 2)
+        
+        return cell
+    }
+    
     
 }
 
---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,13 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Graphene on 22/1/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import Foundation
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
