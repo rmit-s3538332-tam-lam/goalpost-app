4efb853b060f92b9d0143a898036a1e2ee3975ff
Graphene <graphene@Graphene.local>
2019-01-22 14:15:28 +0700

    complete goalCell

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Graphene on 22/1/19.
+//  Copyright © 2019 Graphene. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    
+    func configureCell(description: String, type: String, goalProgressAmount:Int){
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
